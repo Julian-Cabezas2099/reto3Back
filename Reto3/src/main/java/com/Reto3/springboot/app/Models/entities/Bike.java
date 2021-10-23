22b204d5d432d20db3d14c1c21c8b17d959c3f2d
julian-Cabezas20 <julian.cabezas.mt@correo.usa.edu.co>
2021-10-22 19:21:54 -0500

    resultado final

---
+++ Reto3/src/main/java/com/Reto3/springboot/app/Models/entities/Bike.java
@@ -0,0 +1,116 @@
+package com.Reto3.springboot.app.Models.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="bike")
+public class Bike implements Serializable {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String brand;
+    private Integer year;
+    private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "categoryIdFk")
+    @JsonIgnoreProperties("bikes")
+    private Category category;
+
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "bike")
+    // @JsonIgnoreProperties("bike")
+    @JsonIgnoreProperties({"bike","client"})
+    private List<Message> messages;
+
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "bike")
+    @JsonIgnoreProperties({"bike","client"})
+    private List<Reservation> reservations;
+
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    
+
+    
+    
+}
+
